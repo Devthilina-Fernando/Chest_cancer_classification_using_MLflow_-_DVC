f6126d9a2bcd4cdb86f05038178337e47e7d0d0d
Devthilina-Fernando <devthilinajayashan@gmail.com>
2024-05-07 17:12:31 +0530

    model evaluation and DVC added

--- README.md
+++ README.md
@@ -2,12 +2,28 @@
 
 Workflows
 
-Update config.yaml
-Update secrets.yaml [Optional]
-Update params.yaml
-Update the entity
-Update the configuration manager in src config
-Update the components
-Update the pipeline
-Update the main.py
-Update the dvc.yaml
+1) Update config.yaml
+2) Update secrets.yaml [Optional]
+3) Update params.yaml
+4) Update the entity
+5) Update the configuration manager in src config
+6) Update the components
+7) Update the pipeline
+8) Update the main.py
+9) Update the dvc.yaml
+
+## dagshub
+
+MLFLOW_TRACKING_URI=https://dagshub.com/Devthilina-Fernando/Chest_cancer_classification_using_MLflow_-_DVC.mlflow \
+MLFLOW_TRACKING_USERNAME=Devthilina-Fernando \
+MLFLOW_TRACKING_PASSWORD=f6782f6ae3b8a32ed5957cfccf51a5e4e96be535 \
+python script.py
+
+
+Run this to export as env variables:
+
+export MLFLOW_TRACKING_URI=https://dagshub.com/Devthilina-Fernando/Chest_cancer_classification_using_MLflow_-_DVC.mlflow
+
+export MLFLOW_TRACKING_USERNAME=Devthilina-Fernando
+
+export MLFLOW_TRACKING_PASSWORD=f6782f6ae3b8a32ed5957cfccf51a5e4e96be535
--- src/CancerClassifier/config/configuration.py
+++ src/CancerClassifier/config/configuration.py
@@ -1,6 +1,6 @@
 from CancerClassifier.constants import *
-from CancerClassifier.utils.common import read_yaml, create_directories
-from CancerClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig)
+from CancerClassifier.utils.common import read_yaml, create_directories, save_json
+from CancerClassifier.entity.config_entity import (DataIngestionConfig,PrepareBaseModelConfig,TrainingConfig,EvaluationConfig)
 import os
 
 
@@ -72,3 +72,14 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    def get_evaluation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chest-CT-Scan-data",
+            mlflow_uri="https://dagshub.com/Devthilina-Fernando/Chest_cancer_classification_using_MLflow_-_DVC.mlflow",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
